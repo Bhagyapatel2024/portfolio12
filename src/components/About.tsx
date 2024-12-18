6318543e68c05a8ab80a9baf9dc83892d74da702
Bhagyapatel2024 <bhagya290405@gmail.com>
2024-12-18 14:21:34 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,18 +8,7 @@ export default function About() {
         {/* Section Title */}
         <h2 className="text-4xl font-extrabold text-white mb-12 text-center">About Me</h2>
         
-        {/* About Content */}
-{/*         <div className="grid md:grid-cols-2 gap-10 items-center"> */}
-          {/* Image Section */}
-{/*           <div className="flex justify-center">
-            <div className="relative">
-              <img
-                src={t2}
-                alt="Profile"
-                className="rounded-full shadow-xl w-72 h-72 border-4 border-black object-cover transform transition duration-500 hover:scale-105"
-              />
-            </div>
-          </div> */}
+       
 
           {/* Text Section */}
           <div>
@@ -92,7 +81,7 @@ export default function About() {
             </div>
           </div>
         </div>
-      </div>
+
     </section>
   );
 }
